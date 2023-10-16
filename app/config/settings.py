77b6c28433f740062a9ade017566da6276f5a9e0
KayoRonald <kayoronald4321@gmail.com>
2023-10-16 09:42:29 -0300

    chore: update doc Settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,11 +4,11 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
   """arquivo que carrega todas as variáveis de ambiente.
   Esta classe contém as seguintes configurações:
-  * **api_title:** O título da API.
-  * **api_version:** A versão da API.
-  * **api_description:** Uma descrição da API.
-  * **api_contact_name:** O nome da pessoa de contato da API.
-  * **api_contact_email:** O endereço de e-mail da pessoa de contato da API.
+    - *api_title:* O título da API.
+    - *api_version:* A versão da API.
+    - *api_description:* Uma descrição da API.
+    - *api_contact_name:* O nome da pessoa de contato da API.
+    - *api_contact_email:* O endereço de e-mail da pessoa de contato da API.
 
   Estas configurações são carregadas do arquivo `.env` por padrão.
   """
